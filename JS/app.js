783efb8dc399648285a7a34ba243a00939bd33b6
TomasIM <martintomas898@gmail.com>
2024-10-17 16:52:13 -0300

    se agrega responsive al arrow-down y animacion, se corrije margenes del contenido del hero

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,10 @@
 console.log("js conectado");
+
+function scrollDown() {
+    const sectionNovedades = document.querySelector(".container-section__novedades")
+    let disntacia = sectionNovedades.offsetTop;
+    window.scrollBy({
+        top: disntacia -10 , // Desplazarse 100px hacia abajo
+        behavior: 'smooth' // Hacer que el desplazamiento sea suave
+    });
+}
