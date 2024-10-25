6fbcb0ac7f3ff42df8397786275af7344ae04a6d
TomasIM <martintomas898@gmail.com>
2024-10-24 21:34:47 -0300

    se crea box de pago y se arranca su animacion

--- JS/app.js
+++ JS/app.js
@@ -8,3 +8,13 @@ function scrollDown() {
         behavior: 'smooth' // Hacer que el desplazamiento sea suave
     });
 }
+
+function activarInfo(obj) {
+    
+    // alterno class al container de informacion
+    const infoTarjeta = document.querySelector("#info-tarjeta");
+    infoTarjeta.classList.toggle("info-vissibility")
+
+    // alterno class al arrow
+    obj.classList.toggle("arrow-box__click")
+}
