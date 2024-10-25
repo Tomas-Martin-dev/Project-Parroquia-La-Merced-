f3f7db90059348957500cd099f5f73c68be73fc2
TomasIM <martintomas898@gmail.com>
2024-10-25 16:57:06 -0300

    se agregar animacion al box de pago y se agregan icons

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,28 @@
 console.log("js conectado");
-
+const infoTarjeta = document.querySelector("#info-tarjeta");
 function scrollDown() {
     const sectionNovedades = document.querySelector(".container-section__novedades")
     let disntacia = sectionNovedades.offsetTop;
     window.scrollBy({
-        top: disntacia -10 , // Desplazarse 100px hacia abajo
+        top: disntacia - 10, // Desplazarse 100px hacia abajo
         behavior: 'smooth' // Hacer que el desplazamiento sea suave
     });
 }
 
 function activarInfo(obj) {
+    let containersInfo = document.querySelectorAll("#container-info");
+    let containerInfoActual = obj.nextElementSibling;
     
-    // alterno class al container de informacion
-    const infoTarjeta = document.querySelector("#info-tarjeta");
-    infoTarjeta.classList.toggle("info-vissibility")
-
+    // si uno de los container-info esta visible y el container-info que clikeo no lo esta... oculto el que esta visible
+    containersInfo.forEach((div) => {
+    if (div.classList.contains("info-vissibility") && !containerInfoActual.classList.contains("info-vissibility")) {
+        div.classList.toggle("info-vissibility");
+        div.previousElementSibling.classList.toggle("arrow-box__click");
+    }})
+    
+    // alterno class al container-info de informacion
+    containerInfoActual.classList.toggle("info-vissibility");
     // alterno class al arrow
-    obj.classList.toggle("arrow-box__click")
+    obj.classList.toggle("arrow-box__click");
+    
 }
